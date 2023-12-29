3414b0bd23eea16b21a7489b80346f1f6deb6b12
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-12-29 08:49:31 +0000

    tested with icons

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ import time
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.Input(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45,10])
 
 edit_button = sg.Button("Edit")
